f09150e858ef0f5f9854687631257130cb43b3c4
hf <wrom@163.com>
2019-12-09 21:00:33 +0800

    new user

---
+++ src/main/java/com/woniu/pojo/User.java
@@ -0,0 +1,30 @@
+package com.woniu.pojo;
+
+public class User {
+	private Integer uid;
+	private String  uname;
+	private String upsw;
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUpsw() {
+		return upsw;
+	}
+	public void setUpsw(String upsw) {
+		this.upsw = upsw;
+	}
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", uname=" + uname + ", upsw=" + upsw + "]";
+	}
+	
+}
---
+++ src/test/java/com/woniu/test/UserTest.java
@@ -0,0 +1,30 @@
+package com.woniu.test;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.woniu.pojo.User;
+
+public class UserTest {
+
+	@Test
+	public void test() {
+		InputStream is = UserTest.class.getResourceAsStream("/configuration.xml");
+		SqlSessionFactory build = new SqlSessionFactoryBuilder().build(is);
+		SqlSession session = build.openSession();
+		List<User> list = session.selectList("findAll");
+		for (User user : list) {
+			System.out.println(user);
+		}
+		session.close();
+	}
+
+}
